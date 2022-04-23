860fae06720c90b1c6702c1e786ad665ee3faebd
Aryan486 <76522834+Aryan486@users.noreply.github.com>
2022-04-23 14:44:33 +0530

    random session changes

--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const hbs=require("hbs");
 const mongoose=require("mongoose");
-//<<<<<<< HEAD
 const session=require("express-session");
-const MongoDBStore=require('connect-mongodb-session')(session)
-//=======
-//const session=require("express-session")
-//>>>>>>> fdb0494513f9a422d67320bd3f5f4b561a1dd895
+const MongoStore=require('connect-mongodb-session')(session)
 const app=express();
 
 //parse requests using body parser
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-//<<<<<<< HEAD
 //creating storage location for session data
-const store=new MongoDBStore({
+const store=new MongoStore({
     uri:'mongodb://localhost:27017/test',
     collection:'SessionData'
 });
@@ -33,18 +28,7 @@ app.use(session(
         store:store
     }
 ))
-//=======
-//creating session
- app.use(session(
-    {
-        secret:"c'est un secret",
-        resave:false,
-        saveUninitialized:false
-    }
-))
  
-//>>>>>>> fdb0494513f9a422d67320bd3f5f4b561a1dd895
-
 //load routes
 const routes=require("./routes/main");
 const { collection } = require("./models/user");
